1fed75d983648f41eec180454f7c619254214cf5
Kendell Phillip <kphillip1720@gmail.com>
2018-04-07 10:01:31 -0400

    Completed Sharing exercise.

--- S04.03-Exercise-AddMapAndSharing/app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ S04.03-Exercise-AddMapAndSharing/app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -2,7 +2,10 @@ package com.example.android.sunshine;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
@@ -30,5 +33,22 @@ public class DetailActivity extends AppCompatActivity {
     }
 
     // TODO (3) Create a menu with an item with id of action_share
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.detail, menu);
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+        menuItem.setIntent(shareForecastText(mForecast));
+        return true;
+    }
+
     // TODO (4) Display the menu and implement the forecast sharing functionality
+    private Intent shareForecastText(String forecast) {
+        String mimeType = "text/plain";
+
+        Intent shareForecastIntent = ShareCompat.IntentBuilder.from(this)
+                .setType(mimeType)
+                .setText(forecast+FORECAST_SHARE_HASHTAG)
+                .getIntent();
+        return shareForecastIntent;
+    }
 }
